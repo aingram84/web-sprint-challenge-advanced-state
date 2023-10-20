9de62057d625f939d19ef44c47264485b71b7572
Adam Ingram <kr0nai84@gmail.com>
2023-10-19 19:32:08 -0700

    MVP

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { connect } from 'react-redux';
-import { moveClockwise, moveCounterClockwise } from '../state/action-creators';
+import { connect } from 'react-redux'
+import { moveClockwise, moveCounterClockwise } from '../state/action-creators'
 
 export function Wheel(props) {
   const style = [1,2,3,4,5,6]
+  
   return (
     <div id="wrapper">
       <div id="wheel">
-        {style.map((num,idx) => {return(<div key={num} className={idx === props.wheel ? 'cog active':'cog'} style={{"--i":`${idx}`}}> {idx === props.wheel ? 'B':''}
-        </div>)})}
+        {style.map((num,idx) => {
+          return (
+            <div key={num} className={idx === props.wheel ? 'cog active' : 'cog'} style={{ "--i": `${idx}` }}>{idx === props.wheel ? 'B' : ''}</div>
+          )
+        })}
       </div>
       <div id="keypad">
         <button id="counterClockwiseBtn" onClick={() => props.moveCounterClockwise()}>Counter clockwise</button>
@@ -17,5 +21,10 @@ export function Wheel(props) {
     </div>
   )
 }
+const mapStateToProps = (state) => {
+  return {
+    wheel: state.wheel
+  }
+}
 
-export default connect(mapStateToProps, {moveClockwise, moveCounterClockwise})(Wheel);
+export default connect(mapStateToProps,{moveClockwise,moveCounterClockwise})(Wheel)
